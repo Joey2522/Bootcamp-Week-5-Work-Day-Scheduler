05abf827ce6d2d3abb000c81bebca76fd68c545f
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2023-11-28 09:18:21 -0600

    added time to top of page

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(() => {
+
+  let today = dayjs();
+  let formattedDate = today.format('MMM DD, YYYY HH:mm:ss A');
+  console.log(formattedDate);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +26,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  });
 });
