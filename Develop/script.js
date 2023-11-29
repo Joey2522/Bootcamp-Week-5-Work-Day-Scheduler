cedb37c988d5b1e9a8a220a81424e7cd174f3a81
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2023-11-29 17:05:20 -0600

    Everything but my README

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
+//waits until the html and css have loaded before the scripts will render
 $(document).ready(() => {
+  
   //global variables
   const currentTime = dayjs().format("H");
   const localSettings = {};
@@ -7,14 +9,12 @@ $(document).ready(() => {
   //sets date and time
   function updateTime() {
     const today = dayjs();
-
     const formatdDate = today.format('MMM DD, YYYY');
     $('#date').text(formatdDate);
-
     const formatTime = today.format('h:mm:ss A');
     $('#time').text(formatTime);
   }
-
+  //sets the color of the hourly block based off of the time
   function blockColor() {
     $('.time-block').each(function() {
       const blockHour = parseInt(this.id);
@@ -24,6 +24,7 @@ $(document).ready(() => {
       });
     }
     
+  //refreshed the color of the hourly block when the hour changes while you are still on the site  
   function refreshColor() {
     $('.time-block').each(function() {
         const blockHour = parseInt(this.id);
@@ -37,6 +38,7 @@ $(document).ready(() => {
     });
   }
 
+    //saves the entered text to the local storage when the save button is hit
   function textEntry() {
     $('.saveBtn').on('click', function() {
       const key = $(this).parent().attr('id');
@@ -45,15 +47,18 @@ $(document).ready(() => {
     });
   }
 
+  //allows the saved text to remain in the local storage even after a page refresh
   $('.time-block').each(function() {
     const key = $(this).attr('id');
     const value = localStorage.getItem(key);
     $(this).children('.description').val(value);
   });
 
+  //calls the functions
   blockColor();
   textEntry();
   refreshColor();
+
   //Updates the clock in 1 second intervals
   setInterval(updateTime, 1000);
 });
